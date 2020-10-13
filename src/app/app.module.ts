e5b0ff80c1932b8ab04cec2772044774648bbc50
Juan Felipe Calao Pérez <jfcalao@uninorte.edu.co>
2020-10-12 21:43:38 -0500

    un commit mas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaracteristicasComponent } from './caracteristicas/caracteristicas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaracteristicasComponent
   ],
   imports: [
     BrowserModule,
