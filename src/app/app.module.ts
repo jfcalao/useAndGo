ac5cbc645ed2e2986f7f0493361a1584dbeca7f1
Daniel Porto <daporto@uninorte.edu.co>
2020-10-12 21:27:25 -0500

    seccion Porto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiculosComponent
   ],
   imports: [
     BrowserModule,
