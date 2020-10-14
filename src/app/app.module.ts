bac2f0c1c5388512087c21266424aa9f2d025e3c
gguerreroa <gguerreroa@uninorte.edu.co>
2020-10-13 22:52:49 -0500

    Integracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { CaracteristicasComponent } from './caracteristicas/caracteristicas.comp
 import { VehiculosComponent } from './vehiculos/vehiculos.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RegistroComponent } from './registro/registro.component';
+import { FootterComponent } from './footter/footter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VehiculosComponent,
     PaginaPrincipalComponent,
-    NavbarComponent
+    NavbarComponent,
     CaracteristicasComponent,
+    RegistroComponent,
+    FootterComponent,
   ],
   imports: [
     BrowserModule,
